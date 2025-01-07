ff3332feb9752d21b6d9a1d4d8f9b11241bb04cc
Julian-Herrera <juliandavih@gmail.com>
2025-01-07 14:07:09 -0500

    Cápitulo 3: Agregando los estilos de fuentes del proyecto con el archivo font y adicionalmente mejorando la optimización de las imagenes de manera responsiva con el componente <Image />

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './ui/global.css';
+import { inter } from './ui/fonts';
 
 export default function RootLayout({
   children,
@@ -7,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      
+      <body className={`${inter.className} antialiased`}
+      //Permite añadir un estilo de fuente inter a la clase body
+      //Antialiased permite optimizar el tiempo de carga (De Tailwind)
+      >{children}</body>
     </html>
   );
 }
