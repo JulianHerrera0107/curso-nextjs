94b275c0ce69e82fb913eaf33c1216d9c3e0d505
Julian-Herrera <juliandavih@gmail.com>
2025-01-13 10:39:58 -0500

    Cápitulo 16: Añadiendo Metadata. Agregando la API de Metadata en el Layout para mejorar el SEO de nuestro proyecto con un título, descripción y metadatabase para visualizarlo en el navegador en el elemento <head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import './ui/global.css';
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   //Este archivo será considerado el root layout
@@ -11,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      
+
       <body className={`${inter.className} antialiased`}
       //Permite añadir un estilo de fuente inter a la clase body
       //Antialiased permite optimizar el tiempo de carga (De Tailwind)
